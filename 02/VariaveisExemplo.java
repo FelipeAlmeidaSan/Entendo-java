4abe66b04e24eb3afca8bd709e52be97d77fcf55
Felipe Almeida <felipealmeidaux@gmail.com>
2025-07-05 10:36:34 -0300

    Primeiros passos em java, com hello world e variáveis

---
+++ 02/VariaveisExemplo.java
@@ -0,0 +1,17 @@
+
+public class VariaveisExemplo {
+    public static void main(String[] args) {
+        int idade = 28;
+        double altura = 1.75;
+        char genero = 'M';
+        boolean estudante = true;
+        String nome = "Felipe";
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Gênero: " + genero);
+        System.out.println("É estudante? " + estudante);
+    }
+}
+
